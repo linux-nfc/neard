3a9960f34c14ba86eafba2db8cff6aa7555b55df
Samuel Ortiz <sameo@linux.intel.com>
2011-06-06 16:04:58 +0200

    target: Add missing header

---
+++ include/target.h
@@ -0,0 +1,33 @@
+/*
+ *
+ *  neard - Near Field Communication manager
+ *
+ *  Copyright (C) 2011  Intel Corporation. All rights reserved.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License version 2 as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+#ifndef __NEAR_TARGET_H
+#define __NEAR_TARGET_H
+
+#include <stdint.h>
+
+#include <glib.h>
+
+#include <near/tag.h>
+
+struct near_tag *near_target_get_tag(uint32_t target_idx, size_t data_length);
+
+#endif
