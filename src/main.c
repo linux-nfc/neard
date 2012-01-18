aaa0aeea792b89a552ce5ee38bcd78ec91c05926
Olivier Guiter <olivier.guiter@linux.intel.com>
2012-01-18 13:12:00 +0100

    main: Add bluetooth initialization

--- src/main.c
+++ src/main.c
@@ -147,6 +147,7 @@ int main(int argc, char *argv[])
 	__near_adapter_init();
 	__near_ndef_init();
 	__near_manager_init(conn);
+	__near_bluetooth_init();
 
 	__near_plugin_init(option_plugin, option_noplugin);
 
@@ -159,6 +160,7 @@ int main(int argc, char *argv[])
 
 	__near_plugin_cleanup();
 
+	__near_bluetooth_cleanup();
 	__near_manager_cleanup();
 	__near_ndef_cleanup();
 	__near_adapter_cleanup();
