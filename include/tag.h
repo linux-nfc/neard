778ec95d3ccc9a1f5886e0ccda7b1437abb80a7e
Samuel Ortiz <sameo@linux.intel.com>
2012-06-27 11:57:29 +0200

    tag: Remove undefined tag_set_uid routine

--- include/tag.h
+++ include/tag.h
@@ -77,7 +77,6 @@ struct near_tag_driver {
 struct near_tag;
 
 struct near_tag *near_tag_get_tag(uint32_t adapter_idx, uint32_t target_idx);
-int near_tag_set_uid(struct near_tag *tag, uint8_t *uid, size_t uid_length);
 int near_tag_set_ro(struct near_tag *tag, near_bool_t readonly);
 void near_tag_set_blank(struct near_tag *tag, near_bool_t blank);
 int near_tag_add_data(uint32_t adapter_idx, uint32_t target_idx,
