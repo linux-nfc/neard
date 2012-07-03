debdbaea54557c798b0e3e1e8805246a43e1f3ea
Samuel Ortiz <sameo@linux.intel.com>
2012-07-03 23:25:55 +0200

    netlink: Remove undefined but declared target activation routines
    Target activation is done by the kernel when connecting to it.

--- src/near.h
+++ src/near.h
@@ -146,11 +146,6 @@ int __near_netlink_dep_link_up(uint32_t idx, uint32_t target_idx,
 				uint8_t comm_mode, uint8_t rf_mode);
 int __near_netlink_dep_link_down(uint32_t idx);
 int __near_netlink_adapter_enable(int idx, near_bool_t enable);
-int __near_netlink_activate_target(uint32_t adapter_idx,
-					uint32_t target_idx,
-					uint32_t protocol);
-int __near_netlink_deactivate_target(uint32_t adapter_idx,
-					uint32_t target_idx);
 int __near_netlink_init(void);
 void __near_netlink_cleanup(void);
 
