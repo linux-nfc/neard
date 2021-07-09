6e1617c2e4c9c181753816dc14d4878a10cbe9b3
Krzysztof Kozlowski <krzysztof.kozlowski@canonical.com>
2021-07-09 11:51:45 +0200

    ci: add clang builds
    Signed-off-by: Krzysztof Kozlowski <krzysztof.kozlowski@canonical.com>

--- ci/debian.sh
+++ ci/debian.sh
@@ -19,14 +19,20 @@ tzdata tzdata/Zones/Europe select Berlin
 " > /tmp/tzdata-preseed.txt
 debconf-set-selections /tmp/tzdata-preseed.txt
 
+PKGS_CC="build-essential"
+if [ "$CC" = "clang" ]; then
+	PKGS_CC="clang"
+fi
+
+
 apt install -y --no-install-recommends \
 	autoconf \
 	autoconf-archive \
 	automake \
-	build-essential \
 	libdbus-1-dev \
 	libglib2.0-dev \
 	libnl-3-dev \
 	libnl-genl-3-dev \
 	libtool \
-
+	make \
+	$PKGS_CC
