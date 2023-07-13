0dc2b86a30c47db0bf66f23e37b683f872b0bb0e
Dmitry Baryshkov <dmitry.baryshkov@linaro.org>
2023-07-13 18:33:25 +0300

    ci/debian: don't install libc6-dev-ARCH-cross packages
    There is no need to manually install libc6-dev-ARCH-cross packages.
    Corresponding target arch packages will be pulled out via the dependency
    of library dev packages.
    
    Signed-off-by: Dmitry Baryshkov <dmitry.baryshkov@linaro.org>
    Signed-off-by: Krzysztof Kozlowski <krzysztof.kozlowski@linaro.org>

--- ci/debian.cross-compile.sh
+++ ci/debian.cross-compile.sh
@@ -15,12 +15,12 @@ if [ -z "$ARCH" ]; then
 fi
 
 case "$ARCH" in
-	armel) PKGS_CC="gcc-arm-linux-gnueabi libc6-dev-${ARCH}-cross";;
-	arm64) PKGS_CC="gcc-aarch64-linux-gnu libc6-dev-${ARCH}-cross";;
-	ppc64el) PKGS_CC="gcc-powerpc64le-linux-gnu libc6-dev-${ARCH}-cross";;
+	armel) PKGS_CC="gcc-arm-linux-gnueabi";;
+	arm64) PKGS_CC="gcc-aarch64-linux-gnu";;
+	ppc64el) PKGS_CC="gcc-powerpc64le-linux-gnu";;
 	# TODO: libraries for riscv?
-	#riscv64) PKGS_CC="gcc-riscv64-linux-gnu libc6-dev-${ARCH}-cross";;
-	s390x) PKGS_CC="gcc-${ARCH}-linux-gnu libc6-dev-${ARCH}-cross";;
+	#riscv64) PKGS_CC="gcc-riscv64-linux-gnu";;
+	s390x) PKGS_CC="gcc-${ARCH}-linux-gnu";;
 	*) echo "unsupported arch: '$ARCH'!" >&2; exit 1;;
 esac
 
