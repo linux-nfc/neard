3d8599e271871036ab06c2efbe3675df7a762954
Corentin Noël <corentin.noel@collabora.com>
2026-05-06 16:17:12 +0200

    Add meson build system to the CI

---
+++ ci/debian.meson.cross-compile.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+# SPDX-License-Identifier: GPL-2.0
+#
+# Copyright (c) 2018-2020 Petr Vorel <pvorel@suse.cz>
+# Copyright (c) 2021 Canonical Ltd.
+# Author: Krzysztof Kozlowski <krzysztof.kozlowski@canonical.com>
+#                             <krzk@kernel.org>
+# Copyright (c) 2025 Krzysztof Kozlowski <krzk@kernel.org>
+#
+
+set -ex
+
+if [ -z "$ARCH" ]; then
+	echo "missing \$ARCH!" >&2
+	exit 1
+fi
+
+dpkg --add-architecture $ARCH
+apt update
+
+apt install -y --no-install-recommends \
+	libc6-dev-${ARCH}-cross
+
+apt install -y --no-install-recommends \
+	libdbus-1-dev:${ARCH} \
+	libglib2.0-dev:${ARCH} \
+	libnl-3-dev:${ARCH} \
+	libnl-genl-3-dev:${ARCH} \
+	gcc-`dpkg-architecture -a ${ARCH} -q DEB_TARGET_GNU_TYPE`
+
+echo "Install finished: $0"
